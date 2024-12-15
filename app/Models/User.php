4f89de58034195ede3cfb1571b180940568d76c1
XProton03 <radita.septianaipt@gmail.com>
2024-12-15 21:18:38 +0700

    Update akses user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,15 @@ use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+
 
     /**
      * The attributes that are mass assignable.
@@ -45,5 +49,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@cvdeagroup.com');
     }
 }
